559b7be929ea59a9b668ba816fe24599524d5036
yangyu <1727860877@qq.com>
2023-09-18 20:04:24 +0800

    Second commit

---
+++ src/com/learngit/test2.java
@@ -0,0 +1,16 @@
+package com.learngit;
+
+import java.util.HashMap;
+
+/**
+ * @author 阳鱼
+ * @version 1.0
+ */
+public class test2 {
+    //test version2
+    public static void main(String[] args) {
+        HashMap hashMap = new HashMap();
+        hashMap.put(123, 777);
+        System.out.println(hashMap.get(123));
+    }
+}
